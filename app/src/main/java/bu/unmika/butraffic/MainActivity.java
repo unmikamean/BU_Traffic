10eb1515343cabeed6f7b3f0a4e94f6cd79e721c
unmika <unmika2003@gmail.com>
2015-11-14 16:01:56 +0700

    Finish Day1

--- app/src/main/java/bu/unmika/butraffic/MainActivity.java
+++ app/src/main/java/bu/unmika/butraffic/MainActivity.java
@@ -1,6 +1,8 @@
 package bu.unmika.butraffic;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -39,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
                 buttonPlayer.start();
 
                 // Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://www.facebook.com"));
+                startActivity(objIntent);
+
 
 
             } //event
